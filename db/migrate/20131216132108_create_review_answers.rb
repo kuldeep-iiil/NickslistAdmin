1e90f339507d36e39e85e20c22e113874decf8bd
kuldeep-iiil <kuldeep_it2007@yahoo.co.in>
2013-12-18 11:37:43 +0530

    Made changes for change in review functionality

--- db/migrate/20131216132108_create_review_answers.rb
+++ db/migrate/20131216132108_create_review_answers.rb
@@ -4,7 +4,7 @@ class CreateReviewAnswers < ActiveRecord::Migration
       t.column :ReviewID, :integer, :limit => 11, :null => false
       t.column :QuestionID, :integer, :limit => 11, :null => false
       t.column :Comments, :text
-      t.column :IsYes, :string, :limit => 10
+      t.column :IsYes, :string, :limit => 50
       t.column :DateCreated, :datetime, :null => false
       t.column :DateUpdated, :datetime, :null => false
     end

b9c7a60180ad93f64fedd73c1cfed7d8e7742e87
kuldeep-iiil <kuldeep_it2007@yahoo.co.in>
2014-01-15 11:18:32 +0530

    Made changes for change in review functionality

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module NicksListAdminProj
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     #config.i18n.default_locale = :de
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
 
   end
 end

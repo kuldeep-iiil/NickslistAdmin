d25119cee1c90e236bb62089ecfcbce7ab813f08
kuldeep-iiil <kuldeep_it2007@yahoo.co.in>
2014-01-22 14:47:27 +0530

    Made changes for change in review functionality

--- db/migrate/20140120113218_create_site_content_histories.rb
+++ db/migrate/20140120113218_create_site_content_histories.rb
@@ -2,7 +2,7 @@ class CreateSiteContentHistories < ActiveRecord::Migration
   def change
     create_table :site_content_histories do |t|
       t.column :ReportID, :integer, :limit => 11, :null => false
-      t.column :PageCode, :integer, :limit => 9, :null => false
+      t.column :PageCode, :integer, :limit => 11, :null => false
       t.column :Title, :string, :limit => 100, :null => false
       t.column :Content, :text, :null =>false
       t.column :IsEnabled, :boolean, :null => false
